dcf2ffbbe299755ea2cdf573f9ee44652539c8ed
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-15 13:37:32 +0500

    A coyote is chasing a roadrunner and they start out 50 feet apart. If you know how fast they are both traveling, and how far the roadrunner is from safety, determine whether or not the coyote is able to catch the roadrunner. Both animals and the roadrunner's safe place are on a straight line.
    Task:
    Determine whether or not the roadrunner made it to safety.
    
    Input Format:
    Three integer values, the first value represents the distance the roadrunner is from safety, then the roadrunner's speed (feet/second) and then the coyote's speed (feet/second).
    
    Output Format:
    A string that says 'Meep Meep' if the roadrunner made it, or 'Yum' if the coyote caught up to the roadrunner.
    
    Sample Input:
    10
    5
    20
    
    Sample Output:
    Meep Meep

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String FAIL = "Yum";
+    public static final String SUCCESS = "Meep Meep";
+    public static final int coyoteDistance = 50;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int distance = sc.nextInt();
+        int runnerSpeed = sc.nextInt();
+        int coyoteSpeed = sc.nextInt();
+        if (distance/runnerSpeed > (distance+coyoteDistance)/coyoteSpeed) {
+            System.out.println(FAIL);
+        } else
+            System.out.println(SUCCESS);
     }
 }
